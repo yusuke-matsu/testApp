6a0aee66c18e4bb0732fd6057135dcd9b4abf916
yusuke <yusuke.m0902@gmail.com>
2017-01-22 16:07:10 +0900

    mod

--- test.go
+++ test.go
@@ -10,7 +10,7 @@ import (
 	"time"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
-	"github.com/op/go-logging"
+	//"github.com/op/go-logging"
 )
 
 // SimpleChaincode example simple Chaincode implementation
@@ -33,7 +33,7 @@ type IssueSet struct {
 	Issues []Issue `json:"issues"`
 }
 
-var myLogger = logging.MustGetLogger("asset_mgm")
+//var myLogger = logging.MustGetLogger("asset_mgm")
 
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 
@@ -74,15 +74,15 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 			return nil, errors.New("can't parse float")
 		}
 
-		myLogger.Info("start get issue")
+		//myLogger.Info("start get issue")
 		currentBytes, err := t.getIssue(stub, key)
 		fmt.Println(currentBytes)
 		fmt.Println(err)
-		myLogger.Info(currentBytes)
+		//myLogger.Info(currentBytes)
 		//err := json.Unmarshal(currentBytes, &record_issue)
 
 		if err != nil || len(currentBytes) != 0 {
-			myLogger.Info("enter into reged person")
+			//myLogger.Info("enter into reged person")
 
 			regData, err := NewJson(currentBytes)
 			currentAmount, err := regData.Get("amount").Float64()
@@ -97,7 +97,7 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 			return nil, nil
 
 		}
-		myLogger.Info("enter into new person")
+		//myLogger.Info("enter into new person")
 		//Get current date and time
 		t := time.Now()
 
